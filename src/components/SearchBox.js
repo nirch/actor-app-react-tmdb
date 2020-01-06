cc94a7eb369fd309eb9749fffed8cbfb071daf6f
nirch <nirh2@yahoo.com>
2020-01-06 10:06:26 +0200

    creating empty ActorsPage and SearchBox componenets

---
+++ src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class SearchBox extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div>
+               <input/> 
+            </div>
+        )
+    }
+}
---
+++ src/pages/ActorsPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import SearchBox from '../components/SearchBox'
+
+export default class ActorsPage extends Component {
+    
+
+    render() {
+        return (
+            <div>
+                <SearchBox/>
+            </div>
+        )
+    }
+}
