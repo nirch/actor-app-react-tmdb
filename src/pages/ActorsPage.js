c0c73b3401425e98ca670a494d40abd8d035ab0b
nirch <nirh2@yahoo.com>
2020-01-06 11:32:26 +0200

    adding layout to Actors Page

--- src/pages/ActorsPage.js
+++ src/pages/ActorsPage.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import SearchBox from '../components/SearchBox'
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 
 export default class ActorsPage extends Component {
-    
+
     constructor(props) {
         super(props);
 
@@ -17,7 +17,7 @@ export default class ActorsPage extends Component {
     }
 
     searchActors(searchText) {
-        
+
         // let newActorSearchResults = [];
         // for (var i = 0; i < this.state.actorSearchResults.length; i++) {
         //     newActorSearchResults.push(this.state.actorSearchResults[i])
@@ -31,30 +31,79 @@ export default class ActorsPage extends Component {
         if (searchText === "") {
             this.setState({
                 actorSearchResults: []
-            })    
+            })
         } else {
             this.setState({
                 actorSearchResults: this.state.actorSearchResults.concat(searchText)
             })
-    
+
         }
 
     }
 
     addActor(index) {
-        alert( this.state.actorSearchResults[index]);
+        alert(this.state.actorSearchResults[index]);
     }
 
     render() {
         const { actorSearchResults } = this.state;
-        
+
 
         return (
             <div>
                 <Container>
                     <SearchBox searchPlaceholder="Search Actor" results={actorSearchResults}
-                                onSearchChange={this.searchActors} onSelectedResult={this.addActor}/>
-                    <h1>bla bla bla</h1>
+                        onSearchChange={this.searchActors} onSelectedResult={this.addActor} />
+                    <Row>
+                        <Col md={4}>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Container>
             </div>
         )
